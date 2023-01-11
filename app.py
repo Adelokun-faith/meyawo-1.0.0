ff80e4c7e9282270bc4dea9ad2283ce354da8974
faithadelokun2021 <faithadelokun2021@gmail.com>
2023-01-11 13:04:04 +0100

    initial

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, render_template
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/home')
+def home():
+    if request.form == ('POST'):
+        if request.form('send message'):
+            return render_template('login.html')
+    
+    
+    
+if __name__ == '__main__':
+  app.run()
